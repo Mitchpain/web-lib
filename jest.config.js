6b773b66951d9d28ce18f6aabbf891c738c41e79
Mitchpain <michpoud@gmail.com>
2022-04-11 12:49:09 -0400

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
+  modulePathIgnorePatterns: ['<rootDir>/lib/'],
   collectCoverageFrom: ['src/**/*.ts', '!src/index.ts', '!**/*.d.ts'],
 }
